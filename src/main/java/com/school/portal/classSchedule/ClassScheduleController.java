da198c9f08a3dec85128a789436804bf85dea776
eno-conan <eno49study@gmail.com>
2023-03-10 07:20:37 +0900

    #1 授業予定取得機能_生徒ID指定：実コード実装

--- src/main/java/com/school/portal/classSchedule/ClassScheduleController.java
+++ src/main/java/com/school/portal/classSchedule/ClassScheduleController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +14,21 @@ import com.school.portal.util.UseOverFunction;
 
 @RestController
 class ClassScheduleController {
-	
+
 	@Autowired
 	private TargetDateClassesService targetDateClassesService;
-	
+
+	@Autowired
+	private TargetStudentClassesService targetStudentClassesService;
+
 	/**
-	 * 授業一覧取得
+	 * 授業一覧取得_日付指定
 	 * 
-	 * @param dateStr 日付（基本敵に当日の情報）
-	 * @return 更新情報
+	 * @param dateStr 日付（基本的には当日の情報）
+	 * @return 授業一覧
 	 *
 	 */
-//	http://localhost:8080/class-schedule?targetDate=2023-01-23
+	//	http://localhost:8080/class-schedule?targetDate=2023-01-23
 	@GetMapping("/class-schedule")
 	List<Map<String, Object>> getTargetDateClassSchedule(@RequestParam(name = "targetDate") final String dateStr) {
 		Date date = UseOverFunction.convertStrDateToDateType(dateStr);
@@ -35,19 +39,24 @@ class ClassScheduleController {
 			return null;
 		}
 	}
-//	[
-//	  {
-//		    "id": "1",
-//		    "period": "6",
-//		    "grade": "高校1年生",
-//		    "subject": "算数",
-//		    "studentId": "1",
-//		    "studentName": "a",
-//		    "teacherName": "講師A",
-//		    "rescheduleDateStart": "2023/01/23",
-//		    "rescheduleDateEnd": "2023/01/23"
-//		  }
-//		]
 
+	/**
+	 * 授業一覧取得_生徒ID指定
+	 * 
+	 * @param studentId 生徒ID
+	 * @return 授業一覧
+	 *
+	 */
+	//	http://localhost:8080/class-schedule/1
+	@GetMapping("/class-schedule/{studentId}")
+	Map<String, Object> getTargetStudentClassSchedule(
+			@PathVariable(name = "studentId", required = true) final String studentId) {
+		try {
+			return targetStudentClassesService.getTargeStudentClassSchedule(studentId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
