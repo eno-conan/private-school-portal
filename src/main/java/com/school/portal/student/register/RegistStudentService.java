7a0ea33ad5e5324657ee0ce3431fbd702abc745d
eno-conan <eno49study@gmail.com>
2023-03-15 07:56:00 +0900

    #6 生徒関連のController・Serviceクラスにコメント追加

--- src/main/java/com/school/portal/student/register/RegistStudentService.java
+++ src/main/java/com/school/portal/student/register/RegistStudentService.java
@@ -1,3 +1,15 @@
+// -----------------------------------------------------------------------------
+// RegistStudentService.java
+// -----------------------------------------------------------------------------
+//
+// 本ファイルについて
+// * prepareClassroomData()：m_classroomテーブルから教室情報が必要。
+
+// * registerStudent()：生徒の情報をm_studentテーブルに登録する
+//
+// * pickupClassroomInfo():m_classroomテーブルから取得した情報のうち、
+// 画面表示に必要な情報を抽出
+//
 package com.school.portal.student.register;
 
 import java.sql.Timestamp;
@@ -6,10 +18,8 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.school.portal.entity.master.Classroom;
 import com.school.portal.entity.master.Grade;
@@ -18,7 +28,6 @@ import com.school.portal.model.RegistStudentModel;
 import com.school.portal.repository.master.ClassroomRepository;
 import com.school.portal.repository.master.StudentRepository;
 import com.school.portal.util.UseOverFunction;
-
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,68 +35,68 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class RegistStudentService {
 
-	@Autowired
-	private ClassroomRepository classroomRepository;
+  @Autowired
+  private ClassroomRepository classroomRepository;
 
-	@Autowired
-	private StudentRepository studentRepository;
+  @Autowired
+  private StudentRepository studentRepository;
 
-	/**
-	 * // 教室情報取得
-	 * 
-	 * @return 整形した教室情報
-	 *
-	 */
-	public List<Map<String, Object>> prepareClassroomData() {
-		List<Classroom> classrooms = classroomRepository.findAll();
-		if (classrooms.isEmpty()) {
-			log.warn("教室情報0件");
-		} else {
-			log.info("教室情報1件以上");
-		}
+  /**
+   * // 教室情報取得
+   * 
+   * @return 整形した教室情報
+   *
+   */
+  public List<Map<String, Object>> prepareClassroomData() {
+    List<Classroom> classrooms = classroomRepository.findAll();
+    if (classrooms.isEmpty()) {
+      log.warn("教室情報0件");
+    } else {
+      log.info("教室情報1件以上");
+    }
 
-		// 教室IDと教室名のみ取得
-		return pickupClassroomInfo(classrooms);
-	}
+    // 教室IDと教室名のみ取得
+    return pickupClassroomInfo(classrooms);
+  }
 
-	/**
-	 * 生徒登録
-	 * 
-	 * @param content 登録内容
-	 * @throws JsonProcessingException
-	 *
-	 */
-	@Transactional
-	String registerStudent(final RegistStudentModel content) throws JsonProcessingException {
-		Student student = new Student();
-		student.setStudentName(content.getStudentName());
-		student.setBirthday(UseOverFunction.convertStrDateToDateType(content.getBirthDay()));
-		student.setClassroom(new Classroom(content.getClassroomId()));
-		student.setGrade(new Grade(content.getGrade()));
-		student.setDeleteFlg(false);
-		student.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-		student.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
-		studentRepository.save(student);
-		Student registerResult = studentRepository.save(student);
-		return registerResult.getStudentName();
-	}
+  /**
+   * 生徒登録
+   * 
+   * @param content 登録内容
+   * @throws JsonProcessingException
+   *
+   */
+  @Transactional
+  String registerStudent(final RegistStudentModel content) throws JsonProcessingException {
+    Student student = new Student();
+    student.setStudentName(content.getStudentName());
+    student.setBirthday(UseOverFunction.convertStrDateToDateType(content.getBirthDay()));
+    student.setClassroom(new Classroom(content.getClassroomId()));
+    student.setGrade(new Grade(content.getGrade()));
+    student.setDeleteFlg(false);
+    student.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+    student.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+    studentRepository.save(student);
+    Student registerResult = studentRepository.save(student);
+    return registerResult.getStudentName();
+  }
 
-	/**
-	 * // 教室情報取得（整形用）
-	 * 
-	 * @return 必要項目のみ設定した教室情報
-	 *
-	 */
-	private List<Map<String, Object>> pickupClassroomInfo(List<Classroom> classrooms) {
-		List<Map<String, Object>> returnJsonLiteral = new ArrayList<>();
-		for (Classroom info : classrooms) {
-			Map<String, Object> classroomIdAndNameMap = new LinkedHashMap<>();
-			classroomIdAndNameMap.put("classroomId", info.getId());
-			classroomIdAndNameMap.put("prefectureName", info.getMPrefecture().getPrefectureName());
-			classroomIdAndNameMap.put("classroomName", info.getClassroomName());
-			returnJsonLiteral.add(classroomIdAndNameMap);
-		}
-		return Collections.unmodifiableList(returnJsonLiteral);
-	}
+  /**
+   * // 教室情報取得（整形用）
+   * 
+   * @return 必要項目のみ設定した教室情報
+   *
+   */
+  private List<Map<String, Object>> pickupClassroomInfo(List<Classroom> classrooms) {
+    List<Map<String, Object>> returnJsonLiteral = new ArrayList<>();
+    for (Classroom info : classrooms) {
+      Map<String, Object> classroomIdAndNameMap = new LinkedHashMap<>();
+      classroomIdAndNameMap.put("classroomId", info.getId());
+      classroomIdAndNameMap.put("prefectureName", info.getMPrefecture().getPrefectureName());
+      classroomIdAndNameMap.put("classroomName", info.getClassroomName());
+      returnJsonLiteral.add(classroomIdAndNameMap);
+    }
+    return Collections.unmodifiableList(returnJsonLiteral);
+  }
 
 }
--- src/main/java/com/school/portal/student/search/SearchStudentController.java
+++ src/main/java/com/school/portal/student/search/SearchStudentController.java
@@ -1,31 +1,44 @@
+// -----------------------------------------------------------------------------
+// RegistStudentController.java
+// -----------------------------------------------------------------------------
+//
+// 本ファイルについて
+// * searchStudent()：
+// 教室ID・生徒名をQueryパラメータとして受け取り、該当する生徒情報をレスポンスとする
+// ** METHOD:GET
+// ** Path：/student/search
+//
 package com.school.portal.student.search;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.school.portal.exception.StudentSearchException;
 
 @RestController
 class SearchStudentController {
 
-	@Autowired
-	private SearchStudentService searchStudentService;
+  @Autowired
+  private SearchStudentService searchStudentService;
 
-	//	http://localhost:8080/student/search?classroomId=1&studentName=Mike
-	@GetMapping("/student/search")
-	List<Map<String, Object>> searchStudent(@RequestParam(name = "classroomId") final int classroomId,
-			@RequestParam(name = "studentName") final String studentName) {
-		try {
-			return searchStudentService.searchStudent(classroomId, studentName);
-		} catch (StudentSearchException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+  /**
+   * 生徒データ取得
+   * 
+   * @return 該当する生徒一覧
+   */
+  // http://localhost:8080/student/search?classroomId=1&studentName=Mike
+  @GetMapping("/student/search")
+  List<Map<String, Object>> searchStudent(@RequestParam(name = "classroomId") final int classroomId,
+      @RequestParam(name = "studentName") final String studentName) {
+    try {
+      return searchStudentService.searchStudent(classroomId, studentName);
+    } catch (StudentSearchException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 
 }
